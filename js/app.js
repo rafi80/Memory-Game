7c1bfd094ef40eb8eb2ab67113b1eab3f875dc7f
rafi80 <hlebionek@tlen.pl>
2018-05-07 22:25:08 +0200

    Updated app.js. Added list of cards. Added population of shuffled card list in HTML. Added event listeners for clicking card elements in order to show a card

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
 /*
  * Create a list that holds all of your cards
  */
-
+let listOfAllCards = [
+    'fa fa-anchor',
+    'fa fa-anchor',
+    'fa fa-bicycle',
+    'fa fa-bicycle',
+    'fa fa-bolt',
+    'fa fa-bolt',
+    'fa fa-bomb',
+    'fa fa-bomb',
+    'fa fa-cube',
+    'fa fa-cube',
+    'fa fa-diamond',
+    'fa fa-diamond',
+    'fa fa-leaf',
+    'fa fa-leaf',
+    'fa fa-paper-plane-o',
+    'fa fa-paper-plane-o'
+];
 
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
+ *   Use function to:
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+let shuffledListOfCards = shuffle(listOfAllCards);
+createHTMLForCards(shuffledListOfCards);
+
+// Function to find deck list element and populate it with cards using shuffled array of cards
+function createHTMLForCards(array){
+    const deckElement = document.querySelector('.deck');
+    let cardListHTMLItems = '';
+    
+    array.forEach(element => {
+                cardListHTMLItems += ' <li class="card"><i class="' + element + '"></i></li> '
+    });
+
+    deckElement.innerHTML = cardListHTMLItems;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +69,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const deckElement = document.querySelector('.deck'); 
+
+function displayCardsSymbol(evt){
+     evt.target.classList.add("open");
+     evt.target.classList.add("show");
+}
+
+deckElement.addEventListener('click', displayCardsSymbol);
